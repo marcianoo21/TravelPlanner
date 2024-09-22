81789333bfe4c92162c1e768021d4a6510611b28
killu21 <mgrzelak218@gmail.com>
2024-09-22 21:21:49 +0200

    layouts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,8 +10,11 @@ export default function RootLayout() {
   })
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{headerShown: false}}/>
+    <Stack screenOptions={{
+      headerShown: false
+    }}>
+      {/* <Stack.Screen name="index" options={{headerShown: false}}/> */}
+      <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
     </Stack>
   );
 }

7e5ff87492e51acbd012b569c89b55d523846386
killu21 <mgrzelak218@gmail.com>
2024-09-23 15:05:03 +0200

    choose travelers option

---
+++ constants/Options.js
@@ -0,0 +1,30 @@
+export const SelectTravelerList = [
+    {
+        id: 1,
+        title: "Just Me",
+        desc: 'A single traveler in exploration',
+        icon: '✈️',
+        people: '1'
+    },
+    {
+        id: 2,
+        title: "Couple",
+        desc: 'Two travelers in tandem',
+        icon: '🥂',
+        people: '2'
+    },
+    {
+        id: 3,
+        title: "Family",
+        desc: 'A group of fun loving adventures',
+        icon: '👨‍👩‍👦‍👦',
+        people: '3 to 6'
+    },
+    {
+        id: 4,
+        title: "Friends",
+        desc: 'A bunch of thrill-seekers',
+        icon: '🏕️',
+        people: '3 to 10'
+    }
+]

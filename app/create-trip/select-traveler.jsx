b71844161de9c9ec651d4ec4494232d982beb1bd
killu21 <mgrzelak218@gmail.com>
2024-10-06 23:19:18 +0200

    traveler options

--- app/create-trip/select-traveler.jsx
+++ app/create-trip/select-traveler.jsx
@@ -1,13 +1,18 @@
 import { View, Text, TouchableOpacity, FlatList } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { useNavigation, useRouter } from 'expo-router'
 import { Colors } from '../../constants/Colors'
 import { Ionicons } from '@expo/vector-icons'
+import { SelectTravelerList } from '../../constants/Options'
+import OptionCard from '../../components/CreateTrip/OptionCard'
+import { CreateTripContext } from '../../context/CreateTripContext'
 
 
 export default function SelectTraveler() {
 
     const navigation = useNavigation()
+    const [selectedTraveler, setSelectedTraveler] = useState()
+    const {tripData, setTripData} = useContext(CreateTripContext)
 
 
 
@@ -18,6 +23,10 @@ export default function SelectTraveler() {
             headerTransparent: true,
         })
     }, [])
+
+    useEffect(() => {
+      setTripData({...tripData, travelerCount: selectedTraveler})
+    }, [selectedTraveler])
 
   return (
     <View style={{
@@ -44,11 +53,33 @@ export default function SelectTraveler() {
             fontSize: 21,
         }}>Choose your travelers</Text>
 
-        <FlatList>
-            
-        </FlatList>
+        <FlatList
+          data={SelectTravelerList}
+          renderItem={({item, index}) => (
+            <TouchableOpacity
+              onPress={() => setSelectedTraveler(item)}
+             style={{
+              marginVertical: 10,
+            }}>
+              <OptionCard option={item} selectedTraveler={selectedTraveler}/>
+            </TouchableOpacity>
+          )}
+          />
 
       </View>
+      <TouchableOpacity style={{
+        padding: 15, 
+        backgroundColor: Colors.PRIMARY,
+        borderRadius: 15,
+        marginTop: 20
+      }}>
+        <Text style={{
+          textAlign: 'center',
+          color: Colors.WHITE,
+          fontFamily: 'outfit-medium',
+          fontSize: 20,
+        }}>Continue</Text>
+      </TouchableOpacity>
     </View>
   )
 }

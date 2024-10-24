82f881c82e4d8acb78cef92b6ba85b7bb4b63892
killu21 <mgrzelak218@gmail.com>
2024-10-24 02:22:40 +0200

    budget options

--- constants/Options.js
+++ constants/Options.js
@@ -28,3 +28,24 @@ export const SelectTravelerList = [
         people: '3 to 10'
     }
 ]
+
+export const SelectBudgetOptions = [
+    {
+        id: 1,
+        title: "Cheap",
+        desc: 'Budget-friendly trips',
+        icon: '💰',
+    }, 
+    {
+        id: 2,
+        title: "Moderate",
+        desc: 'Balanced trips',
+        icon: '💸',
+    }, 
+    {
+        id: 3,
+        title: "Luxury",
+        desc: 'No expense spared',
+        icon: '💎',
+    },
+]
